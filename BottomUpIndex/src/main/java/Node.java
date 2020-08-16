7bcb9c74b2eefbe9a8b85500012d82a691854dc6
Frank Hulmes <fdhulmes@yahoo.com>
2020-08-16 11:42:43 -0400

    setup for node class

---
+++ BottomUpIndex/src/main/java/Node.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Node {
+
+    private String word;
+    private Integer numOccurences;
+    private ArrayList<Integer> offsets;
+
+    public Node(String word){
+        this.word = word;
+        this.numOccurences = 0;
+        this.offsets = new ArrayList<Integer>();
+    }
+
+    public ArrayList<Integer> getOffsets() {
+        return offsets;
+    }
+
+    public Integer getNumOccurences() {
+        return numOccurences;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void incrementOccurences(){
+        this.numOccurences++;
+    }
+
+    public void addOffset(Integer offset){
+        this.offsets.add(offset);
+    }
+}
